6610ed698df37fbdbd648b5ff200aa2040c926a5
jcguohj <jcguohj.gd@chinatelecom.cn>
2023-06-17 16:24:11 +0800

    init fix

--- android/src/main/java/com/kinder/thermometer/ThermometryModule.java
+++ android/src/main/java/com/kinder/thermometer/ThermometryModule.java
@@ -11,7 +11,7 @@ public class ThermometryModule extends ReactContextBaseJavaModule {
 
     private ReactApplicationContext	mReactContext;
 
-    private ThermometerRecord mThermometerRecord = null;
+    private static ThermometerRecord mThermometerRecord = null;
 
     @Override
     public String getName() {
